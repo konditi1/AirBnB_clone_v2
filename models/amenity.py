217ebfbede41e7d749c71b46f0190b6993a1117a
10thcode <attabadukwabena@gmail.com>
2023-09-25 18:06:40 +0000

    Fix error in place.py and amenity.py

--- models/amenity.py
+++ models/amenity.py
@@ -2,7 +2,6 @@
 """ State Module for HBNB project """
 from models.base_model import BaseModel, Base
 from sqlalchemy.orm import relationship
-from models.place import place_amenity
 from sqlalchemy import Column, String
 
 
@@ -12,7 +11,3 @@ class Amenity(BaseModel, Base):
     """
     __tablename__ = "amenities"
     name = Column(String(128), nullable=False)
-    place_amenities = relationship(
-            "Place",
-            secondary=place_amenity,
-    )
