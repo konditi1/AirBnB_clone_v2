4024fe7a5051d64f05e16711218014b233d06583
10thcode <attabadukwabena@gmail.com>
2023-09-25 16:36:05 +0000

    Update city.py, place.py and user.py

--- models/city.py
+++ models/city.py
@@ -3,6 +3,7 @@
 from models.base_model import BaseModel
 from models.base_model import BaseModel, Base
 from sqlalchemy import Column, String, ForeignKey
+from sqlalchemy import relationship
 
 
 class City(BaseModel, Base):
@@ -10,3 +11,4 @@ class City(BaseModel, Base):
     __tablename__ = "cities"
     name = Column(String(128), nullable=False)
     state_id = Column(String(60), ForeignKey("states.id"))
+    places = relationship("City", backref="cities")
