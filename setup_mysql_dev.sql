0efe59d28ea4b28a2225c03e792f38f675de1342
10thcode <attabadukwabena@gmail.com>
2023-09-25 00:33:21 +0000

    Add setup_mysql_dev.sql
    A SQL script that prepares a MySQL server.

---
+++ setup_mysql_dev.sql
@@ -0,0 +1,6 @@
+-- prepares MySQL server for the AirBnB project
+
+CREATE DATABASE IF NOT EXISTS hbnb_dev_db;
+CREATE USER IF NOT EXISTS 'hbnb_dev'@'localhost' IDENTIFIED BY 'hbnb_dev_pwd';
+GRANT ALL PRIVILEGES ON hbnb_dev_db.* TO hbnb_dev;
+GRANT SELECT ON performance_schema.* TO hbnb_dev;
