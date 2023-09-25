eca1feeb1e81ab91b6245de5b41b7f3ca1a0064b
10thcode <attabadukwabena@gmail.com>
2023-09-25 11:18:06 +0000

    Update city.py and state.py

--- models/state.py
+++ models/state.py
@@ -1,8 +1,23 @@
 #!/usr/bin/python3
 """ State Module for HBNB project """
-from models.base_model import BaseModel
+from models.base_model import BaseModel, Base
+from models.city import City
+from sqlalchemy import Column, String
+from sqlalchemy.orm import relationship
 
 
-class State(BaseModel):
+class State(BaseModel, Base):
     """ State class """
-    name = ""
+    __tablename__ = "states"
+    name = Column(String(128), nullable=False)
+    cities = relationship(City, backref="state")
+
+    @property
+    def cities(self):
+        """ Gets the list of City instances """
+        state_cities = []
+        for city in self.city:
+            if city.state_id == self.id:
+                state_cities.append[city]
+
+        return state_cities
