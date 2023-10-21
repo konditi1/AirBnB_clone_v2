0cccfbe88ae086b53eacb230b9b290f750b2a123
konditi1 <konditi1@gmail.com>
2023-10-21 11:04:44 +0300

    /c/<text>: display “C ” followed by the value of the text variable (replace underscore _ symbols with a space )

---
+++ web_flask/2-c_route.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+""" import Flask from web_flask/__init__.py """
+from flask import Flask, render_template
+app = Flask(__name__)
+@app.route('/', strict_slashes=False)
+def hello_hbnb():
+    """print("Hello HBNB!")"""
+    return "Hello HBNB!"
+
+
+@app.route('/hbnb', strict_slashes=False)
+def hbnb():
+    """print("HBNB")"""
+    return "HBNB"
+
+
+@app.route('/c/<text>', strict_slashes=False)
+def c(text):
+    """print("C ")"""
+    return "C {}".format(text.replace('_', ' '))
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
