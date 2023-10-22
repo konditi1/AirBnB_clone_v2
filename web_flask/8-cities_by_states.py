cd63ca8bcdb1086504a6d8751d58adf7dac77029
konditi1 <konditi1@gmail.com>
2023-10-22 09:14:11 +0300

    task 9

---
+++ web_flask/8-cities_by_states.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+""" City by State Module for HBNB project """
+from models import storage
+from models.state import State
+from flask import Flask, render_template
+app = Flask(__name__)
+
+
+@app.teardown_appcontext
+def teardown():
+    """ Remove the current SQLAlchemy Session """
+    storage.close()
+
+
+@app.route('/cities_by_states', strict_slashes=False)
+def cities_by_states():
+    """ Display a list of States and their Cities """
+    states = sorted(storage.all(State).values(), key=lambda state: state.name)
+    return render_template('8-cities_by_states.html', states=states)
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
